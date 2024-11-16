eaa69445d0958a9b349d9acf5e11e083fe76ebf8
hazemmuuhammed <202027220@std.sci.cu.edu.eg>
2024-11-16 19:40:57 +0200

    Remove Details screen and update navigation routes

--- app/navigation/route.tsx
+++ app/navigation/route.tsx
@@ -2,7 +2,6 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import Home from '../screens/Home';
-import Details from '../screens/Details';
 const Stack = createNativeStackNavigator();
 
 export default function Route() {
@@ -10,7 +9,6 @@ export default function Route() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
   );
